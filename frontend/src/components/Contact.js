6577228d1c60786e1cbc1e555fe7c47e0e5b318e
luigi21dev <ingcivil.luisgomez@gmail.com>
2021-11-16 15:12:26 -0500

    Avance de vista contacto

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,10 +1,59 @@
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import Form from "react-bootstrap/Form"
+import Button from "@restart/ui/esm/Button";
+import '../assets/stylesheets/contact.css'
+
 function Contact() {
+
+  const Formulario = () => {
+    const [datos, setDatos] = useState({
+      nombre: "",
+      apellido: "",
+      correoElectrónico: "",
+      telefono: "",
+      mensaje: ""
+    })
+  }
+
   return (
-      
-      <p>Contact</p>
-    
-    
+    <>
+      <Container className="row" id="Contac-cont">
+        <h2 className="text-center" id="contact-title">
+          Contacta con nosotros
+        </h2>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicName">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control type="text" placeholder="Ingresa tu nombre" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicLastName">
+            <Form.Label>Apellido</Form.Label>
+            <Form.Control type="text" placeholder="Ingresa tu apellido" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Correo electrónico</Form.Label>
+            <Form.Control type="text" placeholder="Ingresa tu correo electrónico" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicphone">
+            <Form.Label>Teléfono</Form.Label>
+            <Form.Control type="number" placeholder="Ingresa tu número telefónico" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicMessage">
+            <Form.Label>Deja aquí tu mensaje</Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder="Ingresa tu mensaje"/>
+          </Form.Group>
+
+          <Button className="btn btn-primary"  variant="primary" type="enviar">
+            Enviar
+          </Button>
+        </Form>
+      </Container>
+    </>
   );
 }
-
 export default Contact;
