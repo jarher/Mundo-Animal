4ffb01acfcc4f8815a127cf123ce61dc42282682
jarher <jefferh33@gmail.com>
2021-12-03 07:38:37 -0500

    cambio puerto http

--- backend/api_app.js
+++ backend/api_app.js
@@ -20,6 +20,16 @@ var app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, '/storage/imgs')));
